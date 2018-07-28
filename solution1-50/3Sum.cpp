2a9fa162e41c460e36783bb1774ba7d46882dcd9
Vious <Vious@users.noreply.github.com>
2018-07-28 22:19:38 +0800

    add 3Sum code

--- solution1-50/3Sum.cpp
+++ solution1-50/3Sum.cpp
@@ -50,32 +50,32 @@ public:
 };
 
 
-int main()
-{
-	Solution sl;
-	vector <int> nums;
-	/*{0,0,0}, {1, 1, -2}, {-2,0,0,2,2}
-	[-1, 0, 1, 2, -1, -4]*/
-
-	nums.push_back(-1);
-	nums.push_back(0);
-	nums.push_back(1);
-	nums.push_back(2);
-	nums.push_back(-1);
-	nums.push_back(-4);
-
-	vector <vector <int>> ret = sl.threeSum(nums);
-	cout << "Answers are: ";
-	for (vector<int> vec : ret){
-		int len = vec.size();
-		for (int i = 0; i < len; i++) {
-			cout << vec[i];
-		}
-		cout << '\n';
-	}
-
-	system("pause");
-
-	return 0;
-}
+//int main()
+//{
+//	Solution sl;
+//	vector <int> nums;
+//	/*{0,0,0}, {1, 1, -2}, {-2,0,0,2,2}
+//	[-1, 0, 1, 2, -1, -4]*/
+//
+//	nums.push_back(-1);
+//	nums.push_back(0);
+//	nums.push_back(1);
+//	nums.push_back(2);
+//	nums.push_back(-1);
+//	nums.push_back(-4);
+//
+//	vector <vector <int>> ret = sl.threeSum(nums);
+//	cout << "Answers are: ";
+//	for (vector<int> vec : ret){
+//		int len = vec.size();
+//		for (int i = 0; i < len; i++) {
+//			cout << vec[i];
+//		}
+//		cout << '\n';
+//	}
+//
+//	system("pause");
+//
+//	return 0;
+//}
 
