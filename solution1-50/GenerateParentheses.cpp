6a1036fad21818bc4a9e9c0621e96426ec32435c
vious <viousxie@outlook.com>
2018-08-01 10:53:26 +0800

    GP

---
+++ solution1-50/GenerateParentheses.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        
+    }
+};
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+
+
+
