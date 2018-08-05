6e78421f7e0c94c5d9a509cb60f8a7171b29b1f9
vious <viousxie@outlook.com>
2018-08-05 20:51:21 +0800

    nP

--- solution1-50/nextPermutation.cpp
+++ solution1-50/nextPermutation.cpp
@@ -11,7 +11,32 @@ public:
         return; */
 
         //my implementation
-        
+        auto len = nums.size();
+        if (len < 2) return;
+        decltype(len) traves = -1;
+        for (decltype(len) i = len - 1; i >= 1; i--) {
+            if (nums[i] > nums[i - 1]) {
+                traves = i;
+                break;
+            }
+        }
+        if (traves == - 1) {
+            sort(nums.begin(), nums.end());
+            return;
+        } else {
+            decltype(len) index;
+            for (decltype(len) i = traves; i >= 0; i--) {
+                if (nums[i] < nums[traves]) {
+                    index = i;
+                    break;
+                }
+            }
+            int tmp = nums[index];
+            nums[index] = nums[traves];
+            nums[traves] = tmp;
+            return ;
+        }
+        return;
     }
 };
 
@@ -19,7 +44,7 @@ public:
 int main(int argc, char const *argv[])
 {
     Solution sl;
-    vector <int> nums = {1, 2, 3};
+    vector <int> nums = {1,3,2};
     sl.nextPermutation(nums);
 
     for (int a : nums) {
